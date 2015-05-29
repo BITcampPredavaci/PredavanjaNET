167647ee4a8398efa783dbe5c8be8df485d238bc
bttalic <b.ttalic@gmail.com>
2015-05-29 13:59:53 +0200

    recap day 5

---
+++ S02D05_Recap/S02D05_Last5/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S02D05_Last5
+{
+    class Program
+    {
+        static void PrintArray(int[] array)
+        {
+            foreach (int el in array)
+            {
+                if (el != -1)
+                {
+                    Console.Write(el + " ");
+                }
+            }
+            Console.WriteLine();
+        }
+
+        static int Sum(int[] array)
+        {
+            int sum = 0;
+            foreach (int a in array)
+            {
+                //sum = sum + a;
+                if (a != -1)
+                {
+                    sum += a;
+                }
+            }
+            return sum;
+        }
+
+        static double Average(int[] array)
+        {
+            int sum = 0;
+
+            int count = 0;
+            foreach (int el in array)
+            {
+                if (el != -1)
+                {
+                    sum += el;
+                    count++;
+                }
+            }
+
+            if (count == 0)
+            {
+                return 0;
+            }
+
+            double average = (double)sum / count;
+            return average;
+        }
+
+        static void ShiftLeft(int[] array)
+        {
+            for (int i = 0; i < array.Length-1; i++)
+            {
+                array[i] = array[i + 1];
+            }
+
+            array[array.Length - 1] = -1;
+        }
+
+        static void InitArray(int[] array, int defaultValue)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = defaultValue;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int input = 0;
+            int[] numbers = new int[5];
+            InitArray(numbers, -1);
+            int index = 0;
+            while (input != -1)
+            {
+                Console.WriteLine("Enter a number, -1 to exit");
+                input = Convert.ToInt32(Console.ReadLine());
+                if (input == -1)
+                {
+                    break;
+                }
+             
+                int lastIndex = index % 5;
+                numbers[lastIndex] = input;
+                index++;
+            }
+
+            PrintArray(numbers);
+            Console.WriteLine("Sum: " + Sum(numbers));
+            Console.WriteLine("Average: " + Average(numbers));
+
+        }
+    }
+}
---
+++ S02D05_Recap/S02D05_Recap/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S02D05_Recap
+{
+    class Program
+    {
+        /// <summary>
+        /// Reverses the string passed as param
+        /// </summary>
+        /// <param name="s">the string to reverse</param>
+        static void Reverse(ref string s){
+
+            string reverse = "";
+
+            for (int i = s.Length-1; i >= 0; i--)
+            {
+                reverse += s[i];
+            }
+            s = reverse;
+        }
+
+         /// <summary>
+        /// Shifts the elements of the passed array by one
+        /// place left, sets the last element to 0
+        /// </summary>
+        /// <param name="array">The array you want to shift</param>
+        static void ShiftLeft(int[] array)
+        {
+            for (int i = 1; i < array.Length; i++)
+            {
+                array[i - 1] = array[i];
+            }
+
+            array[array.Length - 1] = 0;
+        }
+
+        /// <summary>
+        /// Prints the element of the array next to
+        /// each other
+        /// </summary>
+        /// <param name="array">array we want to print</param>
+        static void PrintArray(int[] array)
+        {
+            
+            foreach (int element in array)
+            {
+                Console.Write(element + " ");
+            }
+            Console.WriteLine();
+        }
+
+        static void Main(string[] args)
+        { 
+            string s = "Hello";
+            Console.WriteLine(s);
+            Reverse(ref s);
+            Console.WriteLine(s);
+
+            int[] array = { 1, 2, 3, 4, 5 };
+            PrintArray(array);
+            ShiftLeft(array);
+            PrintArray(array);
+        }
+    }
+}
