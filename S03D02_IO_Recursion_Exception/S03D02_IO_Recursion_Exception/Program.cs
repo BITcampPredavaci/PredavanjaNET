dd324859691be7908dad0f3a032c8ea148bae583
bttalic <b.ttalic@gmail.com>
2015-06-02 20:37:53 +0200

    week 3 day 1

---
+++ S03D02_IO_Recursion_Exception/S03D02_IO_Recursion_Exception/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S03D02_IO_Recursion_Exception
+{
+    class Program
+    {
+
+
+
+        static void PrintToN(int n)
+        {
+            if (n == -1)
+            {
+                return;
+            }
+            Console.WriteLine(n);
+            PrintToN(--n);
+        }
+
+        static void Print1DArray(int[] array)
+        {
+            foreach (int el in array)
+            {
+                Console.Write(el + " ");
+            }
+            Console.WriteLine();
+        }
+
+        static void ChangeToFour(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = 4;
+            }
+        }
+
+        static void Print2DArray(int[][] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                ChangeToFour(array[i]);
+                Print1DArray(array[i]);
+            }
+        }
+
+        static void PrintArrayRec(int[] array, int i  = 0)
+        {
+            if (i == array.Length)
+                return;
+            Console.Write(array[i] + " ");
+            PrintArrayRec(array, i + 1);
+        }
+
+
+        static void Main(string[] args)
+        {
+
+            int[][] array = {
+                  new int[] {1},
+                  new int[] {2, 3},
+                  new int[] {4, 5, 6}
+        };
+
+           // Print2DArray(array);
+
+            PrintArrayRec(array[2]);
+
+        }
+    }
+}
---
+++ S03D02_IO_Recursion_Exception/S03D02_Recap/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S03D02_Recap
+{
+    class Program
+    {
+
+        static bool Equal(int[] array)
+        {
+            int value = array[0];
+            foreach (int el in array)
+            {
+                if (el != value)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        static bool HasEqualRow(int[][] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (Equal(array[i]) == true)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        static void PrintLine(int width, bool firstOrLast)
+        {
+            char edge = '+', middle = '-';
+            if (firstOrLast == false)
+            {
+                edge = '|';
+                middle = ' ';
+            }
+
+            Console.Write(edge);
+
+            for (int i = 0; i < width; i++)
+            {
+                Console.Write(middle);
+            }
+            Console.Write(edge);
+
+            Console.WriteLine();
+        }
+
+        static void PrintSquare(int height, int width)
+        {
+            for (int i = 0; i < height; i++)
+            {
+                bool firstOrLast = false;
+                if (i == 0 || i == height - 1)
+                {
+                    firstOrLast = true;
+                }
+                PrintLine(width, firstOrLast);
+            }
+        }
+
+        static bool CheckColumn(int[][] array, int column)
+        {
+            int value = array[0][column];
+
+            for (int row = 0; row < array.Length; row++)
+            {
+                if (value != array[row][column])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        static bool CheckColumns(int[][] array)
+        {
+            for (int column = 0; column < array[0].Length; column++)
+            {
+                if (CheckColumn(array, column) == true)
+                    return true;
+            }
+            return false;
+        }
+
+        static bool ComplicatedCheck(int[][] array)
+        {
+            for (int column = 0; column < array[0].Length; column++)
+            {
+                bool allEqual = true;
+                int value = array[0][column];
+                for (int row = 0; row < array.Length; row++)
+                {
+                    if (value != array[row][column])
+                    {
+                        allEqual = false;
+                        break;
+                    }
+                }
+                if (allEqual == true)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        static void Main(string[] args)
+        {
+            PrintSquare(5, 10);
+        }
+    }
+}
