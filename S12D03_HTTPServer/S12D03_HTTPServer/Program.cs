5c6ea49759bd71bd7c674a0f204caf63b6479709
bttalic <b.ttalic@gmail.com>
2015-08-05 20:18:12 +0200

    s12d03

---
+++ S12D03_HTTPServer/S12D03_HTTPServer/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace S12D03_HTTPServer
+{
+    class Program
+    { 
+        static void Main()
+        {
+            HttpListener server = new HttpListener();
+            server.Prefixes.Add("http://localhost:7321/");
+            server.Start();
+            HttpListenerContext context =  server.GetContext();
+
+            Console.WriteLine(context.Request.HttpMethod);
+            Console.WriteLine(context.Request.RawUrl);
+            Console.WriteLine(context.Request.UserAgent);
+        }
+       
+    }
+}
---
+++ S12D03_HTTPServer/TCPServer/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace TCPServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /* TcpListener server = new TcpListener(IPAddress.Any, 7321);
+             server.Start();
+             TcpClient client = server.AcceptTcpClient();
+             StreamReader sr = new StreamReader(client.GetStream());
+             Console.WriteLine(sr.ReadLine());
+
+             sr.Close();
+             client.Close();*/
+
+            TcpClient client = new TcpClient("141.101.112.88", 80);
+            StreamReader sr = new StreamReader(client.GetStream());
+            StreamWriter sw = new StreamWriter(client.GetStream());
+
+            sw.WriteLine("GET /");
+            sw.WriteLine("User-Agent:Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.125 Safari/537.36");
+            sw.WriteLine();
+            sw.Flush();
+            while(sr.Peek() > -1)
+            {
+                Console.WriteLine(sr.ReadLine());
+            }
+
+
+        }
+    }
+}
